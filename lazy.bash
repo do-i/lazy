33ec13e6f7ffcbd6b8730404246e6794cc46b033
Ninja <ninja@renton.net>
2025-02-17 08:55:40 -0500

    Use tmp as default dest_dir

--- lazy.bash
+++ lazy.bash
@@ -3,13 +3,13 @@ set -e
 
 # https://github.com/do-i/lazy
 #
-# Usage: ./lazy.bash <dest_dir>
+# Usage: ./lazy.bash [dest_dir]
 #
 dest_dir=${1}
 if [ "$dest_dir" == "" ]; then
-  echo "specify destination dir"
-  exit 1
+  dest_dir="tmp"
 fi
+echo "destination dir: ${dest_dir}"
 
 mkdir -p ${dest_dir}
 cd ${dest_dir}
